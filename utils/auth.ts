8ff88aa0445e0a82e72a700aca6cbc16303f5756
wang miao <miaowang888@outlook.com>
2024-12-05 11:28:32 +0800

    CCCCCCCCCCCCC

--- utils/auth.ts
+++ utils/auth.ts
@@ -7,7 +7,7 @@ export function get_auth_status(context) {
         return false
     }
     var dopath = dopaths[1]
-    if(context.env["GUEST"] && context.request.Method.toUpperCase() == "GET"){
+    if(context.env["GUEST"] && context.request.Method === "get"){
         // if(dopath.startsWith("_$flaredrive$/thumbnails/"))return true;
         const allow_guest = context.env["GUEST"].split(",")
         for (var aa of allow_guest){
